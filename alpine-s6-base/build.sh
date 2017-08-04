e6617b7c3320b8c3de873616acd8cc14d9edcf2d
Jim Razmus II <jim.razmus@code42.com>
2017-08-04 15:54:41 -0500

    Revise labels, add a badge, and make the build record itself.

--- alpine-s6-base/build.sh
+++ alpine-s6-base/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-docker build -t jimrazmus/alpine-s6-base:3.6 \
+
+docker build --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+             -t jimrazmus/alpine-s6-base:3.6 \
              -t jimrazmus/alpine-s6-base:latest .
+
+docker push jimrazmus/alpine-s6-base:3.6
+
+# Tell Microbadger to refresh
+curl -X POST https://hooks.microbadger.com/images/jimrazmus/alpine-s6-base/n_ToWOPzJKS6chmSLn9icnBYSeg=
