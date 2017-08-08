3819ca0e0dd2bff5b5e26fbf342a2a115a3671fe
Jim Razmus II <jim.razmus@code42.com>
2017-08-07 21:04:46 -0500

    Add call to webhook to update microbadger.

--- netbox/build.sh
+++ netbox/build.sh
@@ -10,4 +10,4 @@ docker build --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
 docker push jimrazmus/netbox:${NETBOX}
 
 # Tell Microbadger to refresh
-#curl -X POST https://hooks.microbadger.com/images/jimrazmus/alpine-s6-base/n_ToWOPzJKS6chmSLn9icnBYSeg=
+curl -X POST https://hooks.microbadger.com/images/jimrazmus/netbox/MPCYRezhWoKhcV_LLnmSFag3KD8=
